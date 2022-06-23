c267d25449e64da80fd75863decdcd42d2a79f22
komplektan <komplektan@mail.ru>
2022-06-23 23:59:44 +0500

    Add SnapKit

--- ExampleSnapKit/ViewController.swift
+++ ExampleSnapKit/ViewController.swift
@@ -5,13 +5,29 @@
 //  Created by Вячеслав Квашнин on 23.06.2022.
 //
 
+import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
+    
+    func someFunc() {
+        let button = UIButton()
+        button.backgroundColor = .systemGray
+        button.setTitle("Button", for: .normal)
+        button.layer.cornerRadius = 20
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.width.equalTo(150)
+            make.height.equalTo(50)
+            make.left.equalToSuperview().inset(50)
+            make.top.equalToSuperview().inset(100)
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        someFunc()
+        
     }
 
 
