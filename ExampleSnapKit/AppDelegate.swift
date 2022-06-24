264037f33b14cc38a30fca6b65e467d5f9a701b1
komplektan <komplektan@mail.ru>
2022-06-24 11:35:58 +0500

    Finish constraints

--- ExampleSnapKit/AppDelegate.swift
+++ ExampleSnapKit/AppDelegate.swift
@@ -15,9 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
      
         let window = UIWindow()
-        let vc = ViewController()
-        vc.view.backgroundColor = .white
-        window.rootViewController = vc
+        window.rootViewController = ViewController()
         window.makeKeyAndVisible()
         self.window = window
         
--- ExampleSnapKit/ViewController.swift
+++ ExampleSnapKit/ViewController.swift
@@ -5,28 +5,80 @@
 //  Created by Вячеслав Квашнин on 23.06.2022.
 //
 
-import SnapKit
 import UIKit
+import SnapKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    private let mainView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemCyan
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    private let subView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 10
+        return view
+    }()
+    
+    private let mainImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.cornerRadius = 20
+        imageView.backgroundColor = .systemGray5
+        return imageView
+    }()
+    
+    private let pressButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.backgroundColor = .systemCyan
+        button.layer.cornerRadius = 20
+        button.setTitle("Press", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        someFunc()
-        
+        setup()
+        setupViews()
     }
 
-    func someFunc() {
-        let button = UIButton()
-        button.backgroundColor = .systemGray
-        button.setTitle("Button", for: .normal)
-        button.layer.cornerRadius = 20
-        view.addSubview(button)
-        button.snp.makeConstraints { make in
-            make.width.equalTo(150)
+    private func setup() {
+        view.backgroundColor = .white
+    }
+    
+    private func setupViews() {
+        
+        view.addSubview(mainView)
+        mainView.addSubview(subView)
+        subView.addSubview(pressButton)
+        subView.addSubview(mainImageView)
+        
+        mainView.snp.makeConstraints { make in
+            make.topMargin.left.equalTo(20)
+            make.bottomMargin.right.equalTo(-20)
+        }
+        
+        subView.snp.makeConstraints { make in
+            make.left.top.equalTo(20)
+            make.right.bottom.equalTo(-20)
+        }
+        
+        mainImageView.snp.makeConstraints { make in
+            make.left.equalTo(20)
+            make.right.equalTo(-20)
+            make.height.equalTo(200)
+            make.top.equalTo(subView).inset(20)
+        }
+        
+        pressButton.snp.makeConstraints { make in
+            make.left.equalTo(20)
+            make.right.equalTo(-20)
             make.height.equalTo(50)
-            make.left.equalToSuperview().inset(50)
-            make.top.equalToSuperview().inset(100)
+            make.bottom.equalTo(subView).inset(20)
         }
     }
 }
